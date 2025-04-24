1e792784c4e67303fdf46010fb89b5050482677c
hacktic <foorganders@outlook.com>
2025-04-24 15:36:47 +0100

    Add final round over ui design

--- Assets/Scripts/UI/RoundOverUi.lua
+++ Assets/Scripts/UI/RoundOverUi.lua
@@ -11,11 +11,31 @@ Easing = TweenModule.Easing
 function Init(honeyCollected, tokensEarned)
     _roundOverContainer:Clear()
 
+    local honeyPanicCompleteLabel = UILabel.new()
+    honeyPanicCompleteLabel:SetPrelocalizedText("Round Over!")
+    honeyPanicCompleteLabel:AddToClassList("title-label")
+    honeyPanicCompleteLabel.style.scale = StyleScale.new(Scale.new(Vector2.new(0, 0))) -- Initial scale
+    Timer.new(0.4, function()
+        local labelTween = Tween:new(
+            0.2, -- Start scale
+            1, -- End scale
+            0.25, -- Duration in seconds
+            false, -- Loop flag
+            false, -- Yoyo flag
+            Easing.easeOutBack, -- Easing function
+            function(value, t)
+                honeyPanicCompleteLabel.style.scale = StyleScale.new(Scale.new(Vector2.new(value, value)))
+            end
+        )
+        labelTween:start() -- Start the tween
+    end, false)
+    _roundOverContainer:Add(honeyPanicCompleteLabel)
+
     local honeyCollectedLabel = UILabel.new()
     honeyCollectedLabel:SetPrelocalizedText("Honey Collected: " .. tostring(honeyCollected))
     honeyCollectedLabel:AddToClassList("round-over-label")
     honeyCollectedLabel.style.scale = StyleScale.new(Scale.new(Vector2.new(0, 0))) -- Initial scale
-    Timer.new(0.4, function()
+    Timer.new(0.55, function()
         local labelTween = Tween:new(
             0.2, -- Start scale
             1, -- End scale
@@ -35,7 +55,7 @@ function Init(honeyCollected, tokensEarned)
     tokensEarnedLabel:SetPrelocalizedText("Tokens Earned: " .. tostring(tokensEarned))
     tokensEarnedLabel:AddToClassList("round-over-label")
     tokensEarnedLabel.style.scale = StyleScale.new(Scale.new(Vector2.new(0, 0))) -- Initial scale
-    Timer.new(0.55, function()
+    Timer.new(0.7, function()
         local labelTween = Tween:new(
             0.2, -- Start scale
             1, -- End scale
@@ -54,14 +74,14 @@ function Init(honeyCollected, tokensEarned)
     local continueButton = UIButton.new()
     local continueButtonLabel = UILabel.new()
     continueButtonLabel:SetPrelocalizedText("Continue")
-    continueButtonLabel:AddToClassList("round-over-label")
+    continueButtonLabel:AddToClassList("continue-label")
     continueButton:Add(continueButtonLabel)
     continueButton:AddToClassList("close-button")
     continueButton:RegisterPressCallback(function()
         UIManager.ShowBeeCollectionMode()
     end)
     continueButton.style.scale = StyleScale.new(Scale.new(Vector2.new(0, 0))) -- Initial scale
-    Timer.new(0.7, function()
+    Timer.new(0.85, function()
         local buttonTween = Tween:new(
             0.2, -- Start scale
             1, -- End scale
